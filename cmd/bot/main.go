bb914daad3f75d38517b7671f27f39f6d95c9d3b
David Mebo <krankd@proton.me>
2025-07-22 16:20:29 +0100

    all of it
    originally a one-liner, breaking it down resulted in some issues, the
    rest is AI to save me the time

---
+++ cmd/bot/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"sletish/internal/handlers"
+	"sletish/internal/logger"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	logger.Init()
+	log := logger.Get()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Info("No .env file found, using system environment variables")
+	}
+
+	botToken := os.Getenv("BOT_TOKEN")
+	if botToken == "" {
+		log.Fatal("BOT_TOKEN is required. Set it in .env file or as environment variable")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/webhook", handlers.WebhookHandler(botToken))
+
+	log.Infof("Bot starting on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
